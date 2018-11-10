eb38719b31c9c9d096b4f125d215ac108206e539
Jacob Rammer <jrammer101@gmail.com>
2018-11-09 21:58:57 -0800

    Coded lab per instructions

--- Lab 6/cis122-lab06-grades.py
+++ Lab 6/cis122-lab06-grades.py
@@ -1,26 +1,26 @@
+grades = input("Enter score: ")
+
 # Initialize variables
 count = 0
 average = 0
-low_score = 10000
-high_score = 0
+low_score = grades
+high_score = grades
 total_score = 0
+input_grades = True
 
-# Prompt for grade
-grades = str(input("Enter score: "))
-
-# If the length of grade is greater than 0
-while len(grades) > 0:  # Change this to true or false later
-    total_score = total_score + int(grades)
-    grades = str(input("Enter score: "))
+while len(grades) > 0:
     count += 1
+    total_score = total_score + int(grades)
+    grades = input("Enter score: ")
     average = total_score / count
     if len(grades) == 0:
         print("No score entered")
+        input_grades = False
         break
-    if int(grades) < int(low_score):
+    if int(grades) <= int(low_score):
         low_score = grades
     if int(grades) > int(high_score):
         high_score = grades
 
-
-print(count, low_score, high_score, total_score, average)
+print("*** Results *** ", "\nCount:\t\t", count, "\nAverage:\t", average, "\nLow Score:\t"
+      , low_score, "\nHigh Score:\t", high_score)
