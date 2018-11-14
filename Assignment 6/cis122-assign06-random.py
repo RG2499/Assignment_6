4846c1b6a5f4e971141c8b306695331317175f85
AngryMinion <jrammer101@gmail.com>
2018-11-13 18:05:56 -0800

    Completed project

--- Assignment 6/cis122-assign06-random.py
+++ Assignment 6/cis122-assign06-random.py
@@ -4,18 +4,31 @@ Author: Jacob Rammer
 Partner: None
 Description: Read a number txt file and display results
 '''
+from cis122_assign06_shared import pad_left, pad_right
+import os
 
-fin = open("random_numbers.txt")
+label_spacing = num_spacing = 10  # How far to pad using pad() function
 
-line_count = comments = line_number = total = 0
-for line in fin:  # Counts line in the file
-    if line[0:1] == "#":  # Counts comment lines
-        comments += 1
-    elif line[0:1].isdigit():  # Counts lines that are integers
-        line_count += 1
-        total += int(line)  # Calculates the total of all integers in file
+while True:
+    line_count = comments = line_number = total = 0
+    file_name = input("Enter a file name (blank to exit): ").strip()
+    if len(file_name) == 0:
+        break
 
-print("Count:", line_count)
-print("Comments:", comments)
-print("Total:", total)
-print("Average:", total / line_count)
+    if os.path.exists(file_name):
+        fin = open(file_name)
+
+        for line in fin:  # Counts line in the file
+            if line[0:1] == "#":  # Counts comment lines
+                comments += 1
+            elif line[0:1].isdigit():  # Counts lines that are integers
+                line_count += 1
+                total += int(line)  # Calculates the total of all integers in file
+
+        pad_right("Count:", label_spacing), pad_left(str(line_count), num_spacing)
+        pad_right("Comments:", label_spacing), pad_left(str(comments), num_spacing)
+        pad_right("Total:", label_spacing), pad_left(str(total), num_spacing)
+        pad_right("Average:", label_spacing), pad_left(str(total / line_count), num_spacing)
+
+    else:  # If filename does not exist. Print error and re-prompt
+        print("Invalid filename: ", file_name)
--- Assignment 6/cis122_assign06_shared.py
+++ Assignment 6/cis122_assign06_shared.py
@@ -6,13 +6,32 @@ Description: Python Shared Code Module
 '''
 
 
-def pad_string():
-    pass
+def pad_string(string, space):  # No idea what I'm supposed to do with this
+    """Does nothing at the moment
+    Longer description
+    :param string (STR): String to be formatted
+    :param space (CHR): Inserts space to format
+    :return: Void
+    """
+    print(string.ljust(space))
 
 
-def pad_left():
-    pass
+def pad_left(string, space):  # Pads the string to the leftmost position
+    """Pads left
+    Pads the string to the left of the console
+    :param string (STR): String to be formatted
+    :param space (CHR): Inserts space to format
+    :return: Void
+    """
+    print(string.rjust(space))
 
 
-def pad_right():
-    pass
+def pad_right(string, space):  # Prints the string to the rightmost position
+    """Pads left
+    Pads the string to the left of the console without a newline at the end
+    :param string (STR): String to be formatted
+    :param space (CHR): Inserts space to format
+    :return: Void
+        """
+    print(string.ljust(space), end="")
+
